dae15e2ab9ddee1a35856cc111261e49882ee5fe
zyj <zhongyinjiebit@sina.com>
2022-10-09 13:28:22 +0800

    无符号相加：逆元(求逆元)

--- ch-02/2.3-Integer-Arithmetic/Unsigned-Negation/main.cpp
+++ ch-02/2.3-Integer-Arithmetic/Unsigned-Negation/main.cpp
@@ -10,29 +10,31 @@ using std::string;
 template<unsigned n>
 struct pow2
 {
-	enum {value=2 * pow2<n-1>::value};
+	enum:std::uint64_t{value=2 * pow2<n-1>::value};
 };
 
 template<>
 struct pow2<0>
 {
-	enum {value=1};
+	enum:std::uint64_t {value=1};
 };
 
 template <typename U>
 struct max_unsigned
 {
-	enum {value=pow2<sizeof(U) * 8>::value};
+	enum uint64_t{value=pow2<sizeof(U) * 8>::value};
 };
 
-unsigned negation(unsigned u)
+template<typename U>
+U negation(U u)
 {//modular addtion:negation
-	return 0;
+	if(u==0) return 0;
+	else return max_unsigned<U>::value - u;
 }
 
 int main()
 {
-	//cout << "max_unsigned<unsigned>:" << std::hex 
-	//	<< max_unsigned<unsigned>::value << endl;
+	cout << "negation(2u):" << negation(2u) << endl;
+	cout << "negation(2u) + 2u:" << negation(2u) + 2u << endl;
 	return 0;
 }
